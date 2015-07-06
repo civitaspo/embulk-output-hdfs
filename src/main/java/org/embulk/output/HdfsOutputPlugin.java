53d8bf6b1c27299d2020133699bafb3b83f7e094
takahiro.nakayama <takahiro.nakayama@dena.com>
2015-07-06 23:35:52 +0900

    BUILD SUCCESSFUL

--- src/main/java/org/embulk/output/HdfsOutputPlugin.java
+++ src/main/java/org/embulk/output/HdfsOutputPlugin.java
@@ -1,27 +1,20 @@
 package org.embulk.output;
 
-import org.embulk.config.CommitReport;
-import org.embulk.config.Config;
-import org.embulk.config.ConfigDefault;
-import org.embulk.config.ConfigDiff;
-import org.embulk.config.ConfigSource;
-import org.embulk.config.Task;
-import org.embulk.config.TaskSource;
-import org.embulk.spi.Exec;
-import org.embulk.spi.OutputPlugin;
-import org.embulk.spi.PageOutput;
-import org.embulk.spi.Schema;
-import org.embulk.spi.TransactionalPageOutput;
+import com.google.common.base.Throwables;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.security.UserGroupInformation;
-import org.apache.hadoop.util.Progressable;
+import org.embulk.config.*;
+import org.embulk.spi.*;
+import org.jruby.embed.ScriptingContainer;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 
-public class HdfsOutputPlugin implements OutputPlugin
+public class HdfsOutputPlugin implements FileOutputPlugin
 {
     private static final Logger logger = Exec.getLogger(HdfsOutputPlugin.class);
 
@@ -35,52 +28,179 @@ public class HdfsOutputPlugin implements OutputPlugin
         @ConfigDefault("{}")
         public Map<String, String> getConfig();
 
+        @Config("sequence_format")
+        @ConfigDefault("\".%03d.%02d\"")
+        public String getSequenceFormat();
+
         @Config("output_path")
-        @ConfigDefault("/tmp/")
+        @ConfigDefault("/tmp/embulk.working.hdfs_output.%Y%m%d_%s")
         public String getOutputPath();
 
         @Config("working_path")
-        @ConfigDefault("/tmp/embulk.hdfs_output.%Y%m%d_%s")
+        @ConfigDefault("/tmp/embulk.working.hdfs_output.%Y%m%d_%s")
         public String getWorkingPath();
+
     }
 
     @Override
     public ConfigDiff transaction(ConfigSource config,
-                                  Schema schema,
                                   int taskCount,
-                                  OutputPlugin.Control control)
+                                  FileOutputPlugin.Control control)
     {
         PluginTask task = config.loadConfig(PluginTask.class);
-
-        // retryable (idempotent) output:
-        // return resume(task.dump(), schema, taskCount, control);
-
-        // non-retryable (non-idempotent) output:
-        control.run(task.dump());
-        return Exec.newConfigDiff();
+        return resume(task.dump(), taskCount, control);
     }
 
     @Override
     public ConfigDiff resume(TaskSource taskSource,
-            Schema schema, int taskCount,
-            OutputPlugin.Control control)
+                             int taskCount,
+                             FileOutputPlugin.Control control)
     {
-        throw new UnsupportedOperationException("hdfs output plugin does not support resuming");
+        control.run(taskSource);
+        return Exec.newConfigDiff();
     }
+
 
     @Override
     public void cleanup(TaskSource taskSource,
-            Schema schema, int taskCount,
-            List<CommitReport> successCommitReports)
+                        int taskCount,
+                        List<CommitReport> successCommitReports)
     {
     }
 
     @Override
-    public TransactionalPageOutput open(TaskSource taskSource, Schema schema, int taskIndex)
+    public TransactionalFileOutput open(TaskSource taskSource, final int taskIndex)
     {
         PluginTask task = taskSource.loadTask(PluginTask.class);
 
-        // TODO
-        throw new UnsupportedOperationException("The 'open' method needs to be implemented");
+        Configuration configuration = getHdfsConfiguration(task);
+        FileSystem fs = getFs(configuration);
+        String workingPath = strftime(task.getWorkingPath());
+        return new TransactionalHdfsFileOutput(task, fs, workingPath, taskIndex);
+    }
+
+    private Configuration getHdfsConfiguration(final PluginTask task)
+    {
+        Configuration configuration = new Configuration();
+
+        List configFiles = task.getConfigFiles();
+        for (Object configFile : configFiles) {
+            configuration.addResource(configFile.toString());
+        }
+
+        for (Map.Entry<String, String> entry: task.getConfig().entrySet()) {
+            configuration.set(entry.getKey(), entry.getValue());
+        }
+
+        return configuration;
+    }
+
+    private FileSystem getFs(final Configuration configuration) {
+        try {
+            FileSystem fs = FileSystem.get(configuration);
+            return fs;
+        }
+        catch (IOException e) {
+            logger.error(e.getMessage());
+            throw Throwables.propagate(e);
+        }
+    }
+
+    private String strftime(final String path)
+    {
+        // strftime
+        ScriptingContainer jruby = new ScriptingContainer();
+        Object result = jruby.runScriptlet("Time.now.strftime('" + path + "')");
+        return result.toString();
+    }
+
+    static class TransactionalHdfsFileOutput implements TransactionalFileOutput
+    {
+        private final int taskIndex;
+        private final FileSystem fs;
+        private final String workingPath;
+        private final String sequenceFormat;
+
+        private int fileIndex = 0;
+        private int callCount = 0;
+        private Path currentPath = null;
+        private OutputStream currentStream = null;
+
+        public TransactionalHdfsFileOutput(PluginTask task, FileSystem fs, String workingPath, int taskIndex)
+        {
+            this.taskIndex = taskIndex;
+            this.fs = fs;
+            this.workingPath = workingPath;
+            this.sequenceFormat = task.getSequenceFormat();
+        }
+
+        public void nextFile() {
+            closeCurrentStream();
+            currentPath = new Path(workingPath + String.format(sequenceFormat, taskIndex, fileIndex));
+            try {
+                if (fs.exists(currentPath)) {
+                    // TODO:　appropriate exception
+                    throw Throwables.propagate(new IOException(currentPath.toString() + "already exists."));
+                }
+                currentStream = fs.create(currentPath);
+                logger.info("Uploading '{}'", currentPath.toString());
+            }
+            catch (IOException e) {
+                logger.error(e.getMessage());
+                throw Throwables.propagate(e);
+            }
+            fileIndex++;
+        }
+
+        @Override
+        public void add(Buffer buffer) {
+            if (currentStream == null) {
+                throw new IllegalStateException("nextFile() must be called before poll()");
+            }
+            try {
+                logger.debug("#add called {} times for taskIndex {}", callCount, taskIndex);
+                currentStream.write(buffer.array(), buffer.offset(), buffer.limit());
+                callCount++;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                buffer.release();
+            }
+        }
+
+        @Override
+        public void finish() {
+            closeCurrentStream();
+        }
+
+        @Override
+        public void close() {
+            closeCurrentStream();
+        }
+
+        @Override
+        public void abort() {
+        }
+
+        @Override
+        public CommitReport commit() {
+            CommitReport report = Exec.newCommitReport();
+            report.set("files", currentPath);
+            return report;
+        }
+
+        private void closeCurrentStream() {
+            try {
+                if (currentStream != null) {
+                    currentStream.close();
+                    currentStream = null;
+                }
+
+                callCount = 0;
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+                throw Throwables.propagate(e);
+            }
+        }
     }
 }
