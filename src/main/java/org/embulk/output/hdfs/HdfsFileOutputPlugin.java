46067d5f74bd23c61446323df07b1693933444a1
Civitaspo <civitaspo@gmail.com>
2015-09-12 20:42:34 +0900

    create prototype.
    TODO:
    - README
    - dogfooding

--- src/main/java/org/embulk/output/hdfs/HdfsFileOutputPlugin.java
+++ src/main/java/org/embulk/output/hdfs/HdfsFileOutputPlugin.java
@@ -1,7 +1,14 @@
 package org.embulk.output.hdfs;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
-import com.google.common.base.Optional;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.embulk.config.TaskReport;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
@@ -9,45 +16,48 @@ import org.embulk.config.ConfigDiff;
 import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
+import org.embulk.spi.Buffer;
 import org.embulk.spi.Exec;
 import org.embulk.spi.FileOutputPlugin;
 import org.embulk.spi.TransactionalFileOutput;
+import org.jruby.embed.ScriptingContainer;
+import org.slf4j.Logger;
 
 public class HdfsFileOutputPlugin
         implements FileOutputPlugin
 {
+    private static final Logger logger = Exec.getLogger(HdfsFileOutputPlugin.class);
+
     public interface PluginTask
             extends Task
     {
-        // configuration option 1 (required integer)
-        @Config("option1")
-        public int getOption1();
+        @Config("config_files")
+        @ConfigDefault("[]")
+        public List<String> getConfigFiles();
 
-        // configuration option 2 (optional string, null is not allowed)
-        @Config("optoin2")
-        @ConfigDefault("\"myvalue\"")
-        public String getOption2();
+        @Config("config")
+        @ConfigDefault("{}")
+        public Map<String, String> getConfig();
 
-        // configuration option 3 (optional string, null is allowed)
-        @Config("optoin3")
-        @ConfigDefault("null")
-        public Optional<String> getOption3();
+        @Config("path_prefix")
+        public String getPathPrefix();
 
-        // usually, run() method needs to write multiple files because size of a file
-        // can be very large. So, file name will be:
-        //
-        //    path_prefix + String.format(sequence_format, taskIndex, sequenceCounterInRunMethod) + file_ext
-        //
+        @Config("file_ext")
+        public String getFileNameExtension();
 
-        //@Config("path_prefix")
-        //public String getPathPrefix();
+        @Config("sequence_format")
+        @ConfigDefault("\"%03d.%02d.\"")
+        public String getSequenceFormat();
 
-        //@Config("file_ext")
-        //public String getFileNameExtension();
+        @Config("rewind_seconds")
+        @ConfigDefault("0")
+        public int getRewindSeconds();
 
-        //@Config("sequence_format")
-        //@ConfigDefault("\"%03d.%02d.\"")
-        //public String getSequenceFormat();
+        // this parameter is experimental.
+        @Config("overwrite")
+        @ConfigDefault("false")
+        public boolean getOverwrite();
+
     }
 
     @Override
@@ -56,10 +66,6 @@ public class HdfsFileOutputPlugin
     {
         PluginTask task = config.loadConfig(PluginTask.class);
 
-        // retryable (idempotent) output:
-        // return resume(task.dump(), taskCount, control);
-
-        // non-retryable (non-idempotent) output:
         control.run(task.dump());
         return Exec.newConfigDiff();
     }
@@ -82,12 +88,113 @@ public class HdfsFileOutputPlugin
     @Override
     public TransactionalFileOutput open(TaskSource taskSource, final int taskIndex)
     {
-        PluginTask task = taskSource.loadTask(PluginTask.class);
+        final PluginTask task = taskSource.loadTask(PluginTask.class);
 
-        // Write your code here :)
-        throw new UnsupportedOperationException("HdfsFileOutputPlugin.open method is not implemented yet");
+        final String pathPrefix = strftime(task.getPathPrefix(), task.getRewindSeconds());
+        final String pathSuffix = task.getFileNameExtension();
+        final String sequenceFormat = task.getSequenceFormat();
 
-        // See LocalFileOutputPlugin as an example implementation:
-        // https://github.com/embulk/embulk/blob/master/embulk-standards/src/main/java/org/embulk/standards/LocalFileOutputPlugin.java
+        return new TransactionalFileOutput()
+        {
+            private final List<String> hdfsFileNames = new ArrayList<>();
+            private int fileIndex = 0;
+            private OutputStream output = null;
+
+            @Override
+            public void nextFile()
+            {
+                closeCurrentStream();
+                Path path = new Path(pathPrefix + String.format(sequenceFormat, taskIndex, fileIndex) + pathSuffix);
+                try {
+                    FileSystem fs = getFs(task);
+                    output = fs.create(path, task.getOverwrite());
+                    logger.info("Uploading '{}'", path);
+                }
+                catch (IOException e) {
+                    logger.error(e.getMessage());
+                    throw new RuntimeException(e);
+                }
+                hdfsFileNames.add(path.toString());
+                fileIndex++;
+            }
+
+            @Override
+            public void add(Buffer buffer)
+            {
+                try {
+                    output.write(buffer.array(), buffer.offset(), buffer.limit());
+                }
+                catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                finally {
+                    buffer.release();
+                }
+            }
+
+            @Override
+            public void finish()
+            {
+                closeCurrentStream();
+            }
+
+            @Override
+            public void close()
+            {
+                closeCurrentStream();
+            }
+
+            @Override
+            public void abort()
+            {
+
+            }
+
+            @Override
+            public TaskReport commit()
+            {
+                TaskReport report = Exec.newTaskReport();
+                report.set("hdfs_file_names", hdfsFileNames);
+                return report;
+            }
+
+            private void closeCurrentStream()
+            {
+                if (output != null) {
+                    try {
+                        output.close();
+                        output = null;
+                    }
+                    catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+    }
+
+    private static FileSystem getFs(final PluginTask task)
+            throws IOException
+    {
+        Configuration configuration = new Configuration();
+
+        for (Object configFile : task.getConfigFiles()) {
+            configuration.addResource(configFile.toString());
+        }
+        configuration.reloadConfiguration();
+
+        for (Map.Entry<String, String> entry: task.getConfig().entrySet()) {
+            configuration.set(entry.getKey(), entry.getValue());
+        }
+
+        return FileSystem.get(configuration);
+    }
+
+    private String strftime(final String raw, final int rewind_seconds)
+    {
+        ScriptingContainer jruby = new ScriptingContainer();
+        Object resolved = jruby.runScriptlet(
+                String.format("(Time.now - %s).strftime('%s')", String.valueOf(rewind_seconds), raw));
+        return resolved.toString();
     }
 }
